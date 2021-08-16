e535c3f0ba4c3cf7f8f5dd6d81cc7f27a2a6cce4
Wildan Muhamad Aqso <wildanaqso198@gmail.com>
2021-08-17 05:54:27 +0700

    change title on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function NavBar() {
             <div className="container">
                 <a href="/" className="navbar-brand font-weight-semibold" >
                 <img src={singleLogo}  alt="" width="30" height="30" className="d-inline-block align-text-top me-3" />
-                Movies
+                MovIDN
                 </a>
                 <form className="d-flex">
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
